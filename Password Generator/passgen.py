42397e6a8f2c0e637264469b831086888da61959
Mick Singh <Singh.Mick@outlook.com>
2024-04-25 17:32:46 +1000

    Improved error handling

--- Password Generator/passgen.py
+++ Password Generator/passgen.py
@@ -1,16 +1,19 @@
+# This Python script generates a random password of a specified length by using a combination of letters, digits, and punctuation marks.
 import random
 import string
 
 def generate_password(int_length):    
     alphabet = string.ascii_letters + string.digits + string.punctuation
-    password = ' '.join(random.choice(alphabet) for i in range(int_length))
+    password = ''.join(random.choice(alphabet) for i in range(int_length))
     return password
 
 while True:
     length = input("Enter the length of the password (or press 'Q' to quit) : ")
     if length.lower() == 'q':
         break 
-    final_length = int(length)
-    password = generate_password(final_length)
-    print(f"Generated password : {password}")
-
+    try:
+        final_length = int(length)
+        password = generate_password(final_length)
+        print(f"Generated password : {password}")
+    except ValueError:
+        print("Please enter a valid integer for the password length.")
