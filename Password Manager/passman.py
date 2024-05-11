74625a28ebaa0b4acacb0b7aa0e8457b047a036c
Mick Singh <Singh.Mick@outlook.com>
2024-05-11 17:36:50 +1000

    Improvements

--- Password Manager/passman.py
+++ Password Manager/passman.py
@@ -1,3 +1,5 @@
+# This Python script implements a simple password manager using hashlib for password hashing and getpass for securely entering passwords. It allows users to create an account with a username and password, and then login with the same credentials. The passwords are stored in a dictionary with hashed values for security.
+
 import hashlib
 import getpass
 
@@ -9,7 +11,7 @@ def create_account():
     hashed_password = hashlib.sha256(password.encode()).hexdigest()
     password_manager[username] = hashed_password
     print("Account created successfully!")
-    
+
 def login():
     username = input("Enter your username: ")
     password = getpass.getpass("Enter your password: ")
@@ -17,7 +19,7 @@ def login():
     if username in password_manager.keys() and password_manager[username] == hashed_password:
         print("Login successful!")
     else:
-        print("Invali username or password")
+        print("Invalid username or password")
 
 def main():
     while True:
@@ -29,9 +31,7 @@ def main():
         elif user_choice == "3":
             break
         else:
-            print("Invlid entry, only accepted entires are 1, 2, 3")
-            
-if __name__ ==  "__main__":
+            print("Invalid entry, only accepted entries are 1, 2, 3")
+
+if __name__ == "__main__":
     main()
-        
-    
